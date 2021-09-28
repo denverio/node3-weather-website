cdaa420710b61df8b7ee7ed93ed3b50a05ce636f
Oblea, Denver <denver.oblea@accenture.com>
2021-09-28 11:28:52 +0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,36 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=dcb7b20c38f9635bfcf75fc1840b4759&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  //=//37.8267,-122.4233--lat,long
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          " It is currently " +
+          body.current.temperature +
+          " degrees out." +
+          "It feels like " +
+          body.current.feelslike +
+          " degrees out." +
+          "It feels like " +
+          body.current.feelslike +
+          " degrees out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
